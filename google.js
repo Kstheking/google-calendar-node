36b7f0427720758eedf8df5d95225c6e3c6d0f73
Khushal Sahni <khushalsahni.int@urbancompany.com>
2022-03-26 17:17:00 +0530

    initial commit

---
+++ google.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const fs = require('fs');
+const {google} = require('googleapis');
+
+const app = express();
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+const yourEmail = "youremailhere@gmail.com";
+
+var event = {
+    'summary': 'Google I/O 2015',
+    'location': '800 Howard St., San Francisco, CA 94103',
+    'description': 'A chance to hear more about Google\'s developer products.',
+    'start': {
+      'dateTime': `'2022-04-25T09:00:00-07:00'`,
+      'timeZone': 'America/Los_Angeles',
+    },
+    'end': {
+      'dateTime': '2022-04-25T17:00:00-07:00',
+      'timeZone': 'America/Los_Angeles',
+    },
+    'attendees': [
+      {'email': 'dummyemail@gmail.com'},
+      {'email': 'amotherdummyemail@gmail.com'},
+    ],
+    'reminders': {
+      'useDefault': false,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+      ],
+    },
+  };
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Calendar API.
+  authorize(JSON.parse(content), scheduleMeeting);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.web;
+  oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+}
+
+const scheduleMeeting = (auth) => {
+    const calendar = google.calendar({version: 'v3', auth});
+
+    calendar.events.insert({
+        calendarId: yourEmail,
+        resource: event,
+    }, function(err, event) {
+        if (err) {
+        console.log('There was an error contacting the Calendar service: ' + err);
+        return;
+        }
+        console.log(event.data.htmlLink);
+        console.log(event.data.id);
+    })
+    
+}
+
+const deleteMeeting = (auth, eventId) => {
+    const calendar = google.calendar({version: 'v3', auth});
+
+    calendar.events.delete({
+        calendarId: yourEmail,
+        eventId: eventId
+    }, (err, res) => {
+        if(err) return console.log("error occured ",err) ;
+        console.log(res);
+    })
+}
+
+app.get('/auth/google/callback', (req, res) => {
+    const code = req.query.code;
+    oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log('Token stored to', TOKEN_PATH);
+        });
+        scheduleMeeting(oAuth2Client);
+      });
+
+    res.send("Meeting has been Scheduled!");
+})
+
+app.listen(8000, () => {
+    console.log('Server Started!');
+});
